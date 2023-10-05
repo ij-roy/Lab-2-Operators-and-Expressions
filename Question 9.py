8531d460f6e0237daba37769f8f5763eef6de045
IJ Roy <141866152+ij-roy@users.noreply.github.com>
2023-10-05 19:06:09 +0530

    Update and rename untitled9.py to Question 9.py

---
+++ Question 9.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 18 10:44:48 2023
+
+@author: LAB
+"""
+'''
+9. You are designing a software for a bank. The bank calculates interest on their RD accounts at
+7.1% p.a. You need to develop the functionality, where the user can check their returns for a
+given duration and for a given monthly installment amount. Note that the bank does not allow
+monthly installment less than Rs. 500/- and the duration of RD should be atleast 6 months.
+Check for valid and invalid inputs.
+'''
+#incomplete question
+p = int(input("enter the principal amount: "))
+t = int(input("enter time in months: "))
+
+if p<500 
+    print("amount should not be less than 500")
+    
+    
