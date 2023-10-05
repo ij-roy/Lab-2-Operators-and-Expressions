56c92bf9358b89c7cbdcda1f946354526598b36a
IJ Roy <141866152+ij-roy@users.noreply.github.com>
2023-10-05 19:03:14 +0530

    Update and rename untitled8.py to Question 10.py

--- Question 10.py
+++ Question 10.py
@@ -4,6 +4,11 @@ Created on Fri Aug 18 10:34:20 2023
 
 @author: LAB
 """
+'''
+10. A day has 86,400 secs (24*60*60). Given a number in the range 1 to 86,400, output the current
+time as hours, minutes, and seconds with a 24-hour clock. For example: 70,000 sec is 19 hours,
+26 minutes, and 40 seconds.
+'''
 
 i = int(input("type a number between 1 to 86400:"))
 if i<1 or i>86400:
