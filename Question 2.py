ecdd6f03f80686f43ec7c924fc631d36b7c689b9
IJ Roy <141866152+ij-roy@users.noreply.github.com>
2023-10-05 18:44:39 +0530

    Update and rename untitled1.py to Question 2.py

--- Question 2.py
+++ Question 2.py
@@ -4,6 +4,9 @@ Created on Fri Aug 18 09:56:40 2023
 
 @author: LAB
 """
+'''
+2. Modify the above question to take input in feet and calculate the area in square metres.
+'''
 
 l = float(input("thw length of the plot in feet is:"))
 w = float(input("yhe width of the plot in feet is:"))
