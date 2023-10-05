518fe11d0a541fb1907e9b34fb190bb184285382
IJ Roy <141866152+ij-roy@users.noreply.github.com>
2023-10-05 18:49:22 +0530

    Update and rename untitled4.py to Question 5.py

--- Question 5.py
+++ Question 5.py
@@ -4,6 +4,9 @@ Created on Fri Aug 18 10:16:31 2023
 
 @author: LAB
 """
+'''
+5. Find the area of a Circle of radius r. Only radius between 1 and 100 should be accepted.
+'''
 
 r = int(input("radius of circle is: "))
 if 0<r<100:
